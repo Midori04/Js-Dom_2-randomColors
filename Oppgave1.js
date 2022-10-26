eafad30de7e9dde19b8338be0dcebd1a2ab15107
Midori04 <machineko303@gmail.com>
2022-10-26 13:51:30 +0200

    first commit

---
+++ Oppgave1.js
@@ -0,0 +1,20 @@
+// const cfButton = document.querySelector("#Red").addEventListener("click", function() {
+
+    function myFunction1() {
+        const verdi = document.querySelector("#Color-motto").value;
+        document.body.style.backgroundColor = verdi;
+        document.querySelector("#Color-motto").value = ""
+        
+        document.querySelector("#Text-2").innerHTML = verdi;
+        
+}
+
+
+function myFunction2() {
+    const etNummer = Math.floor(Math.random() * colorList.length)
+    
+    document.body.style.backgroundColor = colorList[etNummer];
+    
+}
+
+const colorList = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey"]
